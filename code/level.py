d10b553dab627e93a5d4d5eb32cb1dc927fbf3e9
spaghetti <bmbowdoin03@gmail.com>
2022-01-29 20:23:37 -0600

    movement

--- code/level.py
+++ code/level.py
@@ -3,6 +3,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -11,7 +12,7 @@ class Level:
 
 
         #setting up sprites
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
     #sprit setup again
         self.create_map()
@@ -22,8 +23,23 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x,y),[self.visible_sprites])
+                    Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x,y), [self.visible_sprites],self.obstacle_sprites)
         
     
     def run(self):
-        pass
+        self.visible_sprites.custom_draw()
+        self.visible_sprites.update()
+        
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image,sprite.rect)
