2e1ae7fd7265e5a4d59a9b86ac557bf2099df983
spaghetti <bmbowdoin03@gmail.com>
2022-01-29 18:19:09 -0600

    dearth

--- code/level.py
+++ code/level.py
@@ -1,4 +1,8 @@
+from re import X
 import pygame
+from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -9,6 +13,16 @@ class Level:
         #setting up sprites
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
+    #sprit setup again
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites])
         
     
     def run(self):
